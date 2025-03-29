4302bb4d7f1f2db380203837640769e66bb5649c
lucas-diet <lucas.d1@gmx.net>
2025-03-29 10:46:53 +0100

    ...

--- script.js
+++ script.js
@@ -18,84 +18,84 @@ function currentDate() {
 
 
 function changeLanguage() {
-    const placeElement = document.getElementById('place');
-    const stateTitle = document.getElementById('currentTitle');
-    const translateButton = document.getElementById('translate');
-    const experienceBtn = document.getElementById('exp');
-    const contactBtn = document.getElementById('contact');
+    const placeElement = document.getElementById("place");
+    const stateTitle = document.getElementById("currentTitle");
+    const translateButton = document.getElementById("translate");
+    const experienceBtn = document.getElementById("exp");
+    const contactBtn = document.getElementById("contact");
 
-    const educElement = document.getElementById('educ');
-    const mrDate = document.getElementById('mrDate')
-    const mr = document.getElementById('mr')
-    const ahDate = document.getElementById('ahDate')
-    const ah = document.getElementById('ah')
-    const bscDate = document.getElementById('bscDate')
-    const bsc = document.getElementById('bsc')
-    const mscDate = document.getElementById('mscDate')
-    const msc = document.getElementById('msc')
+    const educElement = document.getElementById("educ");
+    const mrDate = document.getElementById("mrDate")
+    const mr = document.getElementById("mr")
+    const ahDate = document.getElementById("ahDate")
+    const ah = document.getElementById("ah")
+    const bscDate = document.getElementById("bscDate")
+    const bsc = document.getElementById("bsc")
+    const mscDate = document.getElementById("mscDate")
+    const msc = document.getElementById("msc")
 
-    const expElement = document.getElementById('workLine');
+    const expElement = document.getElementById("workLine");
 
-    const jobSpan1 = document.getElementById('jobSpan1')
-    const jobTitle1 = document.getElementById('jobTitle1');
+    const jobSpan1 = document.getElementById("jobSpan1")
+    const jobTitle1 = document.getElementById("jobTitle1");
 
     // Prüfe, ob die aktuelle Sprache Englisch ist
-    const isEnglish = translateButton.style.backgroundImage.includes('Flag_of_the_United_States.svg');
+    const isEnglish = translateButton.style.backgroundImage.includes("Flag_of_the_United_States.svg");
 
     if (window.location.pathname === "/" || window.location.pathname.includes("index.html")) {
         if (placeElement) {
-            placeElement.textContent = isEnglish ? 'Jena, Germany' : 'Jena, Deutschland';
+            placeElement.textContent = isEnglish ? "Jena, Germany" : "Jena, Deutschland";
         }
         if (stateTitle) {
-            stateTitle.textContent = isEnglish ? 'Bioinformatics student' : 'Student der Bioinformatik';
+            stateTitle.textContent = isEnglish ? "Bioinformatics student" : "Student der Bioinformatik";
         }
         if (experienceBtn) {
-            experienceBtn.textContent = isEnglish ? 'Work Experience' : 'Berufe';
+            experienceBtn.textContent = isEnglish ? "Work Experience" : "Berufe";
         }
         if (contactBtn) {
-            contactBtn.textContent = isEnglish ? 'EMail' : 'E-Mail';
+            contactBtn.textContent = isEnglish ? "EMail" : "E-Mail";
         }
     } 
     else if (window.location.pathname.includes('education.html')) {
         if (educElement) {
-            educElement.textContent = isEnglish ? 'Education' : 'Bildungsweg';
+            educElement.textContent = isEnglish ? "Education" : "Bildungsweg";
         }
         if (mrDate) {
-            mrDate.textContent = isEnglish ? 'Jul/2015' : '07.2015'
+            mrDate.textContent = isEnglish ? "Jul/2015" : "07.2015";
         }
         if (mr) {
-            mr.textContent = isEnglish ? 'Secondary school diploma' : 'Mittlere Reife'
+            mr.textContent = isEnglish ? "Secondary school diploma" : "Mittlere Reife";
         }
         if (ahDate) {
-            ahDate.textContent = isEnglish ? 'Jul/2016' : '07.2016'
+            ahDate.textContent = isEnglish ? "Jul/2016" : "07.2016";
         }
         if (ah) {
-            ah.textContent = isEnglish ? 'High School Diploma' : 'Allgemeine Hochschulreife'
+            ah.textContent = isEnglish ? "High School Diploma" : "Allgemeine Hochschulreife";
         }
         if (bscDate) {
-            bscDate.textContent = isEnglish ? 'Mar/2022' : '03.2022'
+            bscDate.textContent = isEnglish ? "Mar/2022" : "03.2022";
         }
         if (bsc) {
-            bsc.textContent = isEnglish ? 'B. Sc. in Bioinformatics' : 'B. Sc. in Bioinformatik'
+            bsc.textContent = isEnglish ? "B. Sc. in Bioinformatics" : "B. Sc. in Bioinformatk";
         }
         if (mscDate) {
-            mscDate.textContent = isEnglish ? 'Mar/2025' : '03.2025'
+            mscDate.textContent = isEnglish ? "Mar/2025" : "03.2025";
         }
         if (msc) {
-            msc.textContent = isEnglish ? 'M. Sc in Bioinformatics' : 'M. Sc. in Bioinformatik'
+            msc.textContent = isEnglish ? "M. Sc in Bioinformatics" : "M. Sc. in Bioinformatik";
         }
     }
-    else if (window.location.pathname.includes('/experience.html')) {
+    else if (window.location.pathname.includes("experience.html")) {
         if (expElement) {
-            expElement.textContent = isEnglish ? 'Work Experience' : 'Beruflicher Werdegang';
+            expElement.textContent = isEnglish ? "Work Experience" : "Beruflicher Werdegang";
         }
         if (jobSpan1) {
-            jobSpan1.textContent = isEnglish ? 'Oct/2019 - Jul/2020': '10.2019 - 07.2020'
+            jobSpan1.textContent = isEnglish ? "Oct/2019 - Jul/2020" : "10.2019 - 07.2020";
         }
         if (jobTitle1) {
             jobTitle1.innerHTML = isEnglish 
-                ? 'Student Assistant at the Competence & Service Center <br> Friedrich-Schiller-University Jena' 
-                : 'Studentische Hilfskraft im Kompetenz- & Servicezentrum <br> Friedrich-Schiller-Universität Jena';
+                ? "Student Assistant at the Competence & Service Center <br> Friedrich-Schiller-University Jena"
+                : "Studentische Hilfskraft im Kompetenz- & Servicezentrum <br> Friedrich-Schiller-Universität Jena";
         }
     }
 
